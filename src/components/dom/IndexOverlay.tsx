d56c7b9f237d860e4e10b6a880e7fab2dbfb9f62
largefullmoon <largefullmoon@gmail.com>
2024-07-08 13:04:42 -0700

    change profile overview

--- src/components/dom/IndexOverlay.tsx
+++ src/components/dom/IndexOverlay.tsx
@@ -8,17 +8,25 @@ export const IndexOverlay = () => {
     <main className='relative flex flex-col h-full'>
       <div className='flex flex-col items-center justify-between h-full py-36 md:py-32'>
         <span className='text-base font-light pointer-events-none md:text-xl md:relative md:self-start text-white/50 font-ubuntu whitespace-nowrap top-28'>
-          <br />Hello I&apos;m &nbsp;
           <h1 className='inline text-2xl italic normal-case md:text-3xl text-white/60 font-fog'>
-            AI, Three.js, Ecommerce Expert
-          </h1>{' '}
-          <br />An <span className='italic'>AI Python backend and React, TypeScript Frontend</span>&nbsp; Web Developer
-          Based in The Ukraine.
+            As a seasoned Python AI, OCR, RAG, and LLM Backend Expert with extensive experience in React and Vue for frontend development, I bring a robust and diverse skill set to the table. With a solid foundation built over 4 years as an AI backend engineer, I have honed my capabilities in developing and integrating advanced technologies into practical, user-friendly applications.
+            Key Skills:
+            Backend Development: Expertise in Python, AI, OCR (Optical Character Recognition), RAG (Retrieval-Augmented Generation), and LLM (Large Language Models).
+            Frontend Development: Proficiency in React and Vue, creating dynamic, responsive, and interactive user interfaces.
+            Full-Stack Development: Seamless integration of backend and frontend technologies to build comprehensive, scalable, and efficient applications.
+            Project Management: Leading projects from conception to deployment, ensuring alignment with client requirements and industry standards.
+            Notable Projects:
+            Mautic 5.0 Email Saving Issue: Successfully resolved critical email saving issues for registered customers, enhancing the functionality and reliability of the Mautic 5.0 instance.
+            Forex MetaTrader Manager API Integration: Led the integration of the Forex MetaTrader Manager API into web and mobile applications, including developing a WordPress plugin and mobile apps (iOS and Android) with comprehensive features like account management, trading history, and communication tools.
+            Mastodon Docker Instance Management: Managed a Mastodon Docker instance, troubleshooting and resolving significant access issues by addressing complex error logs and ensuring a smooth user experience.
+            Career Objective:
+            My goal is to leverage my expertise in AI, backend, and frontend development to drive innovation and deliver cutting-edge solutions that meet and exceed client expectations. I am passionate about continuous learning and adapting to new technologies, ensuring that my skills and knowledge remain at the forefront of industry advancements.
+          </h1>
         </span>
         <div className='flex flex-col items-center pointer-events-none md:self-end md:items-start'>
           <h2 className='relative italic lg:text-8xl text-[10vw] text-white/60 font-fog right-5'>
-            <span className='relative lg:text-9xl left-2 text-[11vw]'>W</span>
-            eb and Python &nbsp;
+            <span className='relative lg:text-9xl left-2 text-[11vw]'>AI</span>
+            Python Backend and React Frontend &nbsp;
             <span className='lg:text-9xl text-[11vw]'>D</span>e
             <span className='font-light font-ubuntu'>v</span>e
             <span className='font-light font-ubuntu'>l</span>o
@@ -33,7 +41,7 @@ export const IndexOverlay = () => {
             </span> */}
             {/* <button
               onClick={() => router.push('/projects')}
-              className='relative flex items-center self-end text-2xl italic font-light leading-none tracking-wider pointer-events-auto text-white/70 right-5 bottom-2 transition-all font-fog hover-effect'
+              className='relative flex items-center self-end text-2xl italic font-light leading-none tracking-wider transition-all pointer-events-auto text-white/70 right-5 bottom-2 font-fog hover-effect'
             >
               Projects
               <FiArrowUpRight />
